2136fc3427070908d8245a153ad55bed3302892f
Giulio Lodi <lodi.giulio@gmail.com>
2019-03-29 23:32:55 +0100

    Working on firebase

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ var admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 let queue = admin.firestore().collection('queue');
+let matches = admin.firestore().collection('matches');
 
 exports.helloWorld = functions
     .region('europe-west1')
@@ -17,14 +18,16 @@ exports.queuePlayer = functions
     .onRequest((request, response) => {
         let userId = request.query.userId;
         let gfid = Math.floor(Math.random() * 1000) + 1;
-
-        queue.get().then((qs) =>{
+        
+        queue.get().then((qs) => {
             if (qs.empty) {
                 queue.add({
                     time : admin.firestore.Timestamp.now(),
                     uid : userId,
                     gfid : gfid,
                 });
+            } else {
+                initGame();
             }
         });
 
@@ -34,3 +37,12 @@ exports.queuePlayer = functions
             a: 'boh',
         });
     });
+
+function initGame(userId, gfid) {
+    let hostuid = queue.orderBy("time","desc").get().uid;
+    matches.add({
+        hostuid : hostuid,
+        joinuid : userId,
+        gfid : gfid
+    });
+}

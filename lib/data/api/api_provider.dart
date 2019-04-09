e894e71c04f3d3a535b9b2354ffdb9c227fcbd82
ledene <lodi.teo@gmail.com>
2019-04-09 18:28:39 +0200

    prova - rimozione import inutile

--- lib/data/api/api_provider.dart
+++ lib/data/api/api_provider.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-
 import 'package:squazzle/data/models/models.dart';
 import 'net_utils.dart';
 

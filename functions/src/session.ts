167b95835d903f0b7532202888d9bd2d54aa324d
GLodi <lodi.giulio@gmail.com>
2019-08-19 22:20:30 +0200

    Adding enemy moves information in session object

--- functions/src/session.ts
+++ functions/src/session.ts
@@ -1,21 +1,33 @@
 export class Session {
-    matchid: string
-    gfid: string
-    gf: string
-    target: string
-    enemytarget: string
-    moves: number
-    enemyname: string
-    started: boolean
+  matchid: string;
+  gfid: string;
+  gf: string;
+  target: string;
+  moves: number;
+  enemymoves: number;
+  enemyname: string;
+  enemytarget: string;
+  started: boolean;
 
-    constructor(matchid: string, gfid: string, gf: string, target: string, enemytarget: string, moves: number, enemyname: string, started: boolean) {
-        this.matchid = matchid
-        this.gfid = gfid
-        this.gf = gf
-        this.target = target
-        this.enemytarget = enemytarget
-        this.moves = moves
-        this.enemyname = enemyname
-        this.started = started
-    }
+  constructor(
+    matchid: string,
+    gfid: string,
+    gf: string,
+    target: string,
+    moves: number,
+    enemymoves: number,
+    enemyname: string,
+    enemytarget: string,
+    started: boolean
+  ) {
+    this.matchid = matchid;
+    this.gfid = gfid;
+    this.gf = gf;
+    this.target = target;
+    this.moves = moves;
+    this.enemymoves = enemymoves;
+    this.enemyname = enemyname;
+    this.enemytarget = enemytarget;
+    this.started = started;
+  }
 }

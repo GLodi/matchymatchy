68cdfb6e36c2285fa2a2bd4bc6510b9a7d5fc3de
GLodi <lodi.giulio@gmail.com>
2019-09-08 22:53:41 +0200

    Active matches are sent correctly

--- functions/src/get_active_matches.ts
+++ functions/src/get_active_matches.ts
@@ -1,4 +1,5 @@
 import * as admin from "firebase-admin";
+import { QuerySnapshot } from "@google-cloud/firestore";
 import { ActiveMatch } from "./models/active_match";
 
 let matches = admin.firestore().collection("matches");
@@ -7,47 +8,15 @@ let users = admin.firestore().collection("users");
 export async function getActiveMatches(request: any, response: any) {
   let userId: string = request.query.userId;
   try {
-    let list: ActiveMatch[] = [];
     let docs = await users
       .doc(userId)
       .collection("activematches")
       .get();
     if (!docs.empty) {
-      await docs.forEach(async d => {
-        let match = await matches.doc(d.id).get();
-        if (userId == match.data()!.hostuid) {
-          console.log("before push host");
-          list.push(
-            new ActiveMatch(
-              match.id,
-              match.data()!.gfid,
-              match.data()!.hostgf,
-              match.data()!.hosttarget,
-              match.data()!.hostmoves,
-              match.data()!.joinmoves,
-              match.data()!.joinname,
-              match.data()!.jointarget,
-              match.data()!.started
-            )
-          );
-        } else {
-          console.log("before push join");
-          list.push(
-            new ActiveMatch(
-              match.id,
-              match.data()!.gfid,
-              match.data()!.joingf,
-              match.data()!.jointarget,
-              match.data()!.joinmoves,
-              match.data()!.hostmoves,
-              match.data()!.hostname,
-              match.data()!.hosttarget,
-              match.data()!.started
-            )
-          );
-        }
-      });
-      response.send(list);
+      let activeMatches: ActiveMatch[] = await makeList(userId, docs);
+      console.log("length");
+      console.log(activeMatches.length);
+      response.send(activeMatches);
     } else {
       response.status(500).send("No active matches");
     }
@@ -56,4 +25,48 @@ export async function getActiveMatches(request: any, response: any) {
     console.error(e);
     response.status(500).send("Error retrieving active matches information");
   }
+}
+
+async function makeList(
+  userId: string,
+  query: QuerySnapshot
+): Promise<ActiveMatch[]> {
+  let list: ActiveMatch[] = [];
+  for (let doc of query.docs) {
+    let match = await matches.doc(doc.id).get();
+    if (userId == match.data()!.hostuid) {
+      console.log("before push host");
+      console.log(match.id);
+      list.push(
+        new ActiveMatch(
+          match.id,
+          match.data()!.gfid,
+          match.data()!.hostgf,
+          match.data()!.hosttarget,
+          match.data()!.hostmoves,
+          match.data()!.joinmoves,
+          match.data()!.joinname,
+          match.data()!.jointarget,
+          match.data()!.started
+        )
+      );
+    } else {
+      console.log("before push join");
+      console.log(match.id);
+      list.push(
+        new ActiveMatch(
+          match.id,
+          match.data()!.gfid,
+          match.data()!.joingf,
+          match.data()!.jointarget,
+          match.data()!.joinmoves,
+          match.data()!.hostmoves,
+          match.data()!.hostname,
+          match.data()!.hosttarget,
+          match.data()!.started
+        )
+      );
+    }
+  }
+  return list;
 }

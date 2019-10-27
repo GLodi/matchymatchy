8f16386b9417454dc53294d5a892c35ee2ee1c81
GLodi <lodi.giulio@gmail.com>
2019-10-27 18:28:22 +0100

    Forgot to commit exceptions and netutils file

---
+++ lib/data/api/net_utils.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'exceptions.dart';
+
+class NetUtils {
+  static Future<dynamic> get(String url) async {
+    var responseJson;
+    try {
+      final response = await http.get(url);
+      responseJson = _returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet connection');
+    }
+    return responseJson;
+  }
+
+  static dynamic _returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        var responseJson = json.decode(response.body.toString());
+        print(responseJson);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 401:
+      case 403:
+        throw UnauthorisedException(response.body.toString());
+      case 500:
+      default:
+        throw FetchDataException(
+            'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
+    }
+  }
+}
